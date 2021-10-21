bbd67092b7cb79d76a022f2e5dbd2ce3797ec34a
HuyPC2103 <14337@student.vgu.edu.vn>
2021-10-21 22:13:55 +0700

    Remove unnecessary import

--- server/src/main/java/com/bookapp/servlet/BookServlet.java
+++ server/src/main/java/com/bookapp/servlet/BookServlet.java
@@ -1,21 +1,13 @@
 package com.bookapp.servlet;
 
-import com.bookapp.model.Book;
-import com.bookapp.model.Chapter;
-import com.bookapp.serivce.Book.BookService;
 import com.bookapp.servlet.BookHandler.BookHandlerFactory;
 import com.bookapp.servlet.BookHandler.Handler;
 import com.bookapp.util.JSONUtil;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
 
 public class BookServlet extends HttpServlet {
     JSONUtil jsonUtil = new JSONUtil();
